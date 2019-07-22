316fe65c2807170c848118d98bb5d21059a90a0d
Sanelisiwe MADESI <smadesi@c4r16s10.wethinkcode.co.za>
2019-07-22 12:28:25 +0200

    ls_proccessor

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: smadesi <smadesi@student.wethinkcode.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/22 10:55:02 by smadesi           #+#    #+#             */
-/*   Updated: 2019/07/22 11:47:32 by smadesi          ###   ########.fr       */
+/*   Updated: 2019/07/22 12:28:18 by smadesi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,18 +32,71 @@ void init_flags(t_ls_flags *flags)
 	flags->f_t = 0;
 }
 
+void set_flags(char c, t_ls_flags **fs)
+{
+	if (c == 'a')
+		(*fs)->f_a = 1;
+	else if (c == 'R')
+		(*fs)->f_R = 1;
+	else if (c == 't')
+		(*fs)->f_t = 1;
+	else if (c == 'r')
+		(*fs)->f_r = 1;
+	else if (c == 'l')
+		(*fs)->f_l = 1;
+}
+
+
+void	ls_processer(char **s, t_ls_flags *flags)
+{
+	int		i;
+	int		count;
+
+	i = 0;
+	count = 0;
+	while (s[++i])
+	{
+		if (s[i][0] == '-' && ft_isprint(s[i][1]))
+			continue;
+		else
+		{
+			path_handler(s[i], &flags);
+			count++;
+		}
+	}
+	if (!count)
+		path_handler(".", &flags);
+}
+
 int		check_flags(char *s, t_ls_flags *fs)
 {
 	int		i;
+	int		j;
 	char	*s2;
+	int		count;
 
 	i = 0;
+	j = -1;
+	count = 0;
 	s2 = "aRrtl";
 	while (s[++i])
 	{
-
+		while (s2[++j])
+			if (s[i] == s2[j])
+			{
+				set_flags(s[i], &fs);
+				break;
+			}
+		if(ft_strlen(s2) == j)
+		{
+			ft_putstr_fd("ls: invalid option -- \'", 2);
+			ft_putchar_fd(s[i], 2);
+			ft_putstr_fd("\'\n", 2);
+			return (0);
+		}
+		j = -1;
 	}
-
+	return 1;
 }
 
 void	ft_ls(char **str)
@@ -57,30 +110,10 @@ void	ft_ls(char **str)
 	{
 		if (str[i][0] == '-' && ft_isprint(str[i][1])) {
 			if(!check_flags(str[i], &flags))
-			{
-				printf("you suck\n");
 				exit(EXIT_FAILURE);
-			}
 		}
 	}
-	// DIR				*p_dir;
-	// struct dirent	*dep;
-
-	// p_dir = opendir(".");
-	// if (!p_dir) {
-	// 	perror("Could not open dir");
-	// 	exit(EXIT_FAILURE);
-	// 	// ft_putstr_fd("")
-	// }
-
-	// while ((dep = readdir(p_dir)))
-	// {
-	// printf("---> %s\n", dep->d_name);
-
-	// }
-
-
-	// closedir(p_dir);
+	ls_processer(str, &flags);
 }
 
 
